27a26b002d90cc3c72a0e73b04202e37f5f15c35
Blue_Beaker <yyy5566778899@126.com>
2020-06-20 18:32:51 +0800

    Modified windows batch, added linux script

---
+++ Pack112.sh
@@ -0,0 +1,4 @@
+cd ./"BlueBlocksCraft 1.12.2"/
+zip -0 -FS -r BlueBlocksCraft-1.12.zip *
+cd ..
+mv ./"BlueBlocksCraft 1.12.2"/BlueBlocksCraft-1.12.zip ./
---
+++ Pack114.sh
@@ -0,0 +1,4 @@
+cd ./"BlueBlocksCraft 1.14"/
+zip -0 -FS -r BlueBlocksCraft-1.14.zip *
+cd ..
+mv ./"BlueBlocksCraft 1.14"/BlueBlocksCraft-1.14.zip ./
