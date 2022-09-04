77a5bf25339421815eafd3d975b4e3fd6395b683
blue-beaker <yyy5566778899@126.com>
2022-09-04 13:41:38 +0800

    Various texture changes

--- Pack116.sh
+++ Pack116.sh
@@ -1,4 +1,5 @@
+#! /bin/bash
 cd ./"BlueBlocksCraft 1.16"/
-zip -0 -FS -r BlueBlocksCraft-1.16.zip *
+zip -0 -FS -r BlueBlocksCraft-1.16.zip * -x='*.pdn' -x='*.png~'
 cd ..
 mv ./"BlueBlocksCraft 1.16"/BlueBlocksCraft-1.16.zip ./
