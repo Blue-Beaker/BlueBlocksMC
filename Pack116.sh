b7d9d14505fc709a9ac6e963453b09d2d6494cb0
blue-beaker <yyy5566778899@126.com>
2024-08-26 21:58:24 +0800

    reduce file size

--- Pack116.sh
+++ Pack116.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd ./"BlueBlocksCraft 1.16"/
-zip -0 -FS -r BlueBlocksCraft-1.16.zip * -x='*.pdn' -x='*.png~' -x='*.cubik' -x='*deprecated*'
+zip -1 -FS -r BlueBlocksCraft-1.16.zip * -x='*.pdn' -x='*.png~' -x='*.cubik' -x='*deprecated*'
 cd ..
 mv ./"BlueBlocksCraft 1.16"/BlueBlocksCraft-1.16.zip ./
