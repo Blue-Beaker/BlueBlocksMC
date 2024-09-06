386112b5c873ac98414b7424110eb34392548c08
blue-beaker <yyy5566778899@126.com>
2024-09-06 23:39:23 +0800

    1.12 icons and appleskin support

--- Pack112.sh
+++ Pack112.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd ./"BlueBlocksCraft 1.12.2"/
-zip -1 -FS -r BlueBlocksCraft-1.12.zip * -x='*.pdn' -x='*.png~' -x='*.cubik' -x='*deprecated*'
+zip -1 -FS -r BlueBlocksCraft-1.12.zip * -x='*.pdn' -x='*.kra' -x='*.kra~' -x='*.png~' -x='*.cubik' -x='*deprecated*'
 cd ..
 mv ./"BlueBlocksCraft 1.12.2"/BlueBlocksCraft-1.12.zip ./
--- Pack116.sh
+++ Pack116.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 cd ./"BlueBlocksCraft 1.16"/
-zip -1 -FS -r BlueBlocksCraft-1.16.zip * -x='*.pdn' -x='*.png~' -x='*.cubik' -x='*deprecated*'
+zip -1 -FS -r BlueBlocksCraft-1.16.zip * -x='*.pdn' -x='*.kra' -x='*.kra~' -x='*.png~' -x='*.cubik' -x='*deprecated*'
 cd ..
 mv ./"BlueBlocksCraft 1.16"/BlueBlocksCraft-1.16.zip ./
